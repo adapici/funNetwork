333a24109508ecb4960872e32133f8fadcfb062b
Lauren Colbert <lauren.colbert26@gmail.com>
2017-07-27 15:42:38 -0500

    please god work

---
+++ network.py
@@ -0,0 +1,27 @@
+
+Register = input("Welcome to Jellyfishin'!!!!!!!! \nWould you like to Sign in (s) or Register (r)?")
+
+class profile:
+    def _init_(self, user_name):
+        self.user_name = user_name
+        self.friends = []
+
+    #def post(self, post):
+        #self.post.append(post) #if it doesn't work use extend
+
+    #def friends(self, friends):
+        #self.friends.extend(friends) #if doesn't work put append
+
+    #def addUser(self, user_name):
+        #self.users.append(profile(user_name))
+
+
+        def main():
+         _init_(self,  user_name)
+        User1 = profile("post" , "friends")
+
+if Register == "r":
+    user_name = input("what's your username?")
+
+    profile.addUser("user_name","")
+    Register== input("What's your username?")
